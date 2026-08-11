84a0ba4e1c5a06132dfe3c9dc604af7bd297c665
Enmanuel Ochoa <eochoa0007@gmail.com>
2026-08-11 08:38:28 -0500

    Creación de ruta get directa

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 //Incializar la aplicación
 const app = express()
 const port = 3026
+
+    app.get ('/hola', (req: Request, res: Response) => {
+        res.send('Hola Mundo!')
+    })
 
 
 app.listen(port, () =>{
