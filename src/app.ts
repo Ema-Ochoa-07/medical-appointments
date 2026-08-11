0135a648ca861a93a258caaf10142e0da950689d
Enmanuel Ochoa <eochoa0007@gmail.com>
2026-08-11 09:08:13 -0500

    Se agregó el middleware para poder leer formatos json que vienen en el body del front request

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,24 @@ import express, { Request, Response } from 'express'
 const app = express()
 const port = 3026
 
+
+
+//MIDDLEWARE: Permite recibir datos en formato JSON
+    app.use(express.json());
+
+
+//METODOS GET-POST-PUT-DELETE-PATCH    
     app.get ('/hola', (req: Request, res: Response) => {
-        res.send('Hola Mundo!')
+        res.send('Method Get!')
     })
 
+    
+    app.post('/', (req, res)=>{
+        console.log(req.body)
+        res.send("Method Post")
+    })
 
+    
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`)
 } )
