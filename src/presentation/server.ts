4e52616af4aba4f38091407425b4a69ebc7d2676
Enmanuel Ochoa <eochoa0007@gmail.com>
2026-08-11 12:53:31 -0500

    Primera configuración del proyecto server.ts y limpiado de app.ts

---
+++ src/presentation/server.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+
+interface Options {
+    port: number
+}
+
+export class Server{
+    
+    private readonly port: number
+    private readonly app = express()
+
+    constructor(options: Options){
+        this.port = options.port
+    }
+
+
+    /*Método (start) PARA LANZAR LA APLICACIÓN
+    Este método va hacer AZÍNCRONO para que se vuelva una promesa
+    */
+    async start(){
+        //Middleware 
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({extended: true})) //Con esto angular puede recibir paquetes en un formato reconocible
+
+        this.app.listen(this.port,() =>{
+            console.log(`Server is running on port ${this.port} 😎`)
+        })
+    }
+}
+
