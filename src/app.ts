1418e71924575af7f9e4359505209e441f783ee6
Enmanuel Ochoa <eochoa0007@gmail.com>
2026-08-11 19:12:30 -0500

    El server ya corre desde el llamado de app

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ async function main() {
     //Instanciar la clase -> En este caso el SERVER
     const server = new Server({port: 3000})
 
+    //AHORA QUE YA TENGO ACCESO A LOS MÉTODOS DE SERVER
     //Devolución de la promesa del método ASYNCRONO DE START -> PÚBLICO
     await server.start()
 
