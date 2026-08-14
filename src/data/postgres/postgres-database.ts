ce1ab44613c0b1c8e167a0a7e95808a2d587ff83
Enmanuel Ochoa <eochoa0007@gmail.com>
2026-08-14 10:38:37 -0500

    Instalación y config TYPE_ORM - configuración postgres-database.ts

---
+++ src/data/postgres/postgres-database.ts
@@ -0,0 +1,36 @@
+import { DataSource } from "typeorm";
+
+interface Options {
+    host: string,
+    port: number,
+    username: string,
+    password: string,
+    database: string
+}
+
+export class PostgresDatabase{
+    private datasource: DataSource
+
+    constructor(options: Options) {
+        this.datasource = new DataSource({
+            type:"postgres",
+            host: options.host,
+            port: options.port,
+            username: options.username,
+            password: options.password,        
+            database: options.database,
+            //entities
+
+            synchronize: true   // En producción no se recomienda SYNCRHONIZE sino migraciones
+        })
+    }
+
+    async connect(){
+        try {
+            await this.datasource.initialize()
+            console.log('Connected to database 👌')
+        } catch(error){
+            console.log(error)
+        }
+    }
+}
